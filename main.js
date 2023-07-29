16a66b289595dc9d4843022e3e4551fa38b00e97
Nahuel Cuesta <nahuelgcuesta@gmail.com>
2023-07-29 10:14:45 -0300

    Se elimino la pagina talentos y se coloco todo el contenido en el index, tambien se agregaron filtros con JS para filtrar segun profesion por boton y mostrarlo en el dom

--- main.js
+++ main.js
@@ -1,43 +1,35 @@
-const talentos = [];
+document.addEventListener("DOMContentLoaded", function () {
+    const botonActriz = document.querySelector("#boton-actriz");
+    const botonActor = document.querySelector("#boton-actor");
+    const botonPromotor = document.querySelector("#boton-promotor");
+    const botonPromotora = document.querySelector("#boton-promotora");
+    const contenedorTalentos = document.querySelector("#lista-talentos");
 
-const listaTalentos = document.querySelector("#lista-talentos");
+    function filtrarTalentos(palabraClave) {
+        const regex = new RegExp(`\\b${palabraClave}\\b`, "i");
 
+        contenedorTalentos.querySelectorAll(".contenedor-talento").forEach(function (elemento) {
+            if (regex.test(elemento.textContent)) {
+                elemento.style.display = "flex";
+            } else {
+                elemento.style.display = "none";
+            }
+        });
+    }
 
-//LeerBotones
+    botonActriz.addEventListener("click", function () {
+        filtrarTalentos("actriz");
+    });
 
-const botonActriz = document.querySelector("#boton-actriz");
+    botonActor.addEventListener("click", function () {
+        filtrarTalentos("actor");
+    });
 
-const botonActor = document.querySelector("#boton-actor");
+    botonPromotor.addEventListener("click", function () {
+        filtrarTalentos("promotor");
+    });
 
-const botonPromotor = document.querySelector("#boton-promotor");
-
-const botonPromotora = document.querySelector("#boton-promotora");
-
-//escucharEventos
-
-botonActriz.addEventListener('click', mostrarActrices);
-botonActor.addEventListener('click', mostrarActores);
-botonPromotor.addEventListener('click', mostrarPromotores);
-botonPromotora.addEventListener('click', mostrarPromotoras);
-
-//dibujar
-
-const contenedorTalentosSeleccionados = document.querySelector("#contenedor-talentos-seleccionados");
-
-//funciones
-
-function mostrarActrices( evt){
-    console.log(evt);
-}
-function mostrarActores ( evt){
-    console.log(evt);
-}
-
-function mostrarPromotores( evt){
-    console.log(evt);
-}
-
-function mostrarPromotoras( evt){
-    console.log(evt);
-}
-
+    botonPromotora.addEventListener("click", function () {
+        filtrarTalentos("promotora");
+    });
+});
